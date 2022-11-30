c7241cf1b400118d109c4b785318aaabe31fed9c
Jake <jakeroggenbuck2@gmail.com>
2022-11-30 15:02:44 -0800

    Add some much needed comments

--- src/main.c
+++ src/main.c
@@ -5,21 +5,25 @@
 
 void derive(double (*func)(double)) {
     printf("x,y,y'\n");
+    // Calculate different values for the derivative
+    // Going from 0.001, 0.002, 0.003 ... 0.001 * 1000
     for (int i = -SIZE; i < SIZE; i++) {
+        // input x
         double x = i * H;
+        // ( f(x + H) - f(x) ) / H
         double k = (func((double)x + H) - func(x)) / H;
+        // f(x)
         double y = func(x);
         printf("%f,%f,%f\n", x, y, k);
     }
 }
 
 double function(double x) {
-	// f(x) = x^3 * 4 + 2
-	return (pow(x, 3) * 4) + 2;
+    // f(x) = x^3 * 4 + 2
+    return (pow(x, 3) * 4) + 2;
 }
 
 int main() {
-
     derive(function);
     return 0;
 }
